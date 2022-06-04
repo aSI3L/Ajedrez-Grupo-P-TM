586673d80fefa6689808586c87cca4f0b3a4056e
Asiel <asiel.alvarez@alumnos.frm.utn.edu.ar>
2022-06-04 16:59:17 -0300

    Primer Commit / Subida de Archivos

---
+++ ajedrez.py
@@ -0,0 +1,40 @@
+#Inicialización
+
+
+#Juego
+
+
+#Funciones
+
+
+#Peon
+
+
+#Caballo
+
+
+#Rey
+
+
+#Alfil
+
+
+#Torre
+
+
+#Reina
+
+
+#Funciones, Ataques, Evaluaciones
+
+
+#Puntajes
+
+
+#Reglas
+
+
+#Evaluaciones de Movimientos / Árbol Binario
+
+
+#Modos de Juego
